2e2bc1103f273bb7ab3261dd9bcf97c977c39f10
Nicole de Guzman <nicoledeguz@hotmail.com>
2021-12-27 01:10:08 -0500

    HTML markup, plus a few things here n there :sparkles::ribbon::sparkles:

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,20 @@
-$(document).ready(function(){
-    $('.gallery').slick({
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false,
-        cssEase: 'ease-out',
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 7
-              }
-            },
-            {
-              breakpoint: 1050,
-              settings: {
-                slidesToShow: 6,
-              }
-            },
-            {
-              breakpoint: 875,
-              settings: {
-                slidesToShow: 5,
-              }
-            },
-            {
-              breakpoint: 725,
-              settings: {
-                slidesToShow: 4,
-              }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 300,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 175,
-                settings: {
-                  slidesToShow: 1,
-                }
-            }
-          ]
-    });
+const form = document.getElementById("my-form");
+    
+async function handleSubmit(e) {
+  e.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(e.target);
+  fetch(e.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Thanks for keeping in touch!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "OOP– Let's try that again!"
   });
+}
+form.addEventListener("submit", handleSubmit)
